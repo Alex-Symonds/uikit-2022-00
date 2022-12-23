991bd49db1325f26d5628245ef450e891c9a89ae
Alex Symonds <alex-symonds@gmail.com>
2022-12-23 13:36:16 +0000

    Button: Replaced cssVariables with enums, to streamline passing variables

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -1,5 +1,51 @@
 import {css} from 'styled-components';
 
+export const enum PALETTE{
+    primary = "#6E41E2",
+    primaryFaint = "rgba(110, 65, 226, .04)",
+    primaryMedium = "rgba(110, 65, 226, .16)",
+    primaryStrong = "rgba(110, 65, 226, .56)",
+
+    disabled = "#E3DAF9",
+    hover = "#5835B0",
+    active = "#472C8A",
+
+    white = "#FFFFFF",
+    whiteExtraFaint = "rgba(255, 255, 255, .12)",
+    whiteFaint = "rgba(255, 255, 255, .16)",
+    whiteMedium = "rgba(255, 255, 255, .56)",
+    whiteStrong = "rgba(255, 255, 255, .88)",
+
+    grayM = "#FAFAFA",
+    grayL = "#F1F1F1",
+    grayS = "#D4D4D4",
+    grayK = "#6B6B6B",
+    graySMedium = "rgba(212, 212, 212, .48)",
+
+    black = "#111",
+    blackFaint = "rgba(17, 17, 17, .04)",
+    blackMedium = "rgba(17, 17, 17, .12)",
+    blackHeavy = "rgba(17, 17, 17, .56)",
+
+    redGirl = "#F9E3E3",
+    redBoy = "#F9E3E3",
+    red = "#DB524E",
+    redSuper = "#C7302B",
+    green = "#27AE60",
+
+    code = "#428BF9",
+}
+
+export enum FONT{
+    main = "Roboto, Arial, Helvetica, sans-serif",
+    heading = "Montserrat, Tahoma, sans-serif"
+}
+
+export enum EFFECTS{
+    shadow = "0 4px 4px 0 rgba(51, 51, 51, 0.04), 0 4px 16px 0 rgba(51, 51, 51, 0.08)"
+}
+
+
 export const cssVariables = css`
     --colorPrimary: #6E41E2;
     --colorPrimaryFaint: rgba(110, 65, 226, .04);

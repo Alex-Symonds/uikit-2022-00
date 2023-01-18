677155ec03ea9f06c097b35f3c3a6245219b66e0
Alex Symonds <alex-symonds@gmail.com>
2023-01-18 14:42:46 +0000

    Added Files component, supports picker and drag-and-drop. Also added Montserrat font and support.

---
+++ components/CircleAroundIcon.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import {PALETTE} from './Theme';
+
+const StyledDiv = styled.div<Pick<CircleAroundIconProps, "color" | "size">>`
+    border-radius: 50%;
+    background: ${ props => props.color };
+    height: ${ props => props.size };
+    width: ${ props => props.size };
+
+    box-sizing: border-box;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+interface CircleAroundIconProps{
+    color?: string,
+    size?: string,
+    children: React.ReactNode
+}
+
+export default function CircleAroundIcon({color, size, children} : CircleAroundIconProps){
+    color = color === undefined ? PALETTE.grayL : color;
+    size = size === undefined ? "3.25rem" : size;
+
+    return (
+        <StyledDiv size={size} color={color}>
+            {children}
+        </StyledDiv>
+    )
+}
+
--- components/Theme.tsx
+++ components/Theme.tsx
@@ -1,9 +1,4 @@
-
-import { url } from 'inspector';
 import { css } from 'styled-components';
-
-
-
 
 export const enum PALETTE{
     primary = "#6E41E2",
@@ -31,6 +26,7 @@ export const enum PALETTE{
     black = "#111",
     blackFaint = "rgba(17, 17, 17, .04)",
     blackMedium = "rgba(17, 17, 17, .12)",
+    blackStrong = "rgba(17, 17, 17, .48)",
     blackHeavy = "rgba(17, 17, 17, .56)",
 
     redGirl = "#F9E3E3",
@@ -44,12 +40,14 @@ export const enum PALETTE{
 
 export const enum FONT{
     main = "Roboto, Arial, Helvetica, sans-serif",
-    heading = "Montserrat, Tahoma, sans-serif"
+    headingBold = "MontserratBold, Tahoma, sans-serif",
+    headingBlack = "MontserratBlack, Tahoma, sans-serif"
 }
 
 export const enum SHADOW{
     default = "0 4px 4px 0 rgba(51, 51, 51, 0.04), 0 4px 16px 0 rgba(51, 51, 51, 0.08)",
     contextMenu = "0 4px 4px 0 rgba(51, 51, 51, 0.04), 0 14px 24px 0 rgba(51, 51, 51, 0.12)",
+    hoverFile = "0 4px 4px 0 rgba(51, 51, 51, 0.04), 0 4px 24px 0 rgba(51, 51, 51, 0.24)",
 }
 
 export const enum LAYOUT{
@@ -85,3 +83,60 @@ export const p3 = css`
     font-size: 0.8125rem;
     line-height: 1.25rem;
 `;
+
+export const TYPOGRAPHY = {
+    h1: css`
+        font-family: ${FONT.headingBlack};
+        font-size: 3.5rem;
+        font-weight: 900;
+        line-height: 4rem;
+    `,
+    h2: css`
+        font-family: ${FONT.headingBold};
+        font-size: 3rem;
+        font-weight: bold;
+        line-height: 3.5rem;
+    `,
+    h3: css`
+        font-family: ${FONT.headingBold};
+        font-size: 2.5rem;
+        font-weight: bold;
+        line-height: 3rem;
+    `,
+    h4: css`
+        font-family: ${FONT.headingBold};
+        font-size: 2rem;
+        font-weight: bold;
+        line-height: 2.5rem;
+    `,
+    h5: css`
+        font-family: ${FONT.headingBold};
+        font-size: 1.5rem;
+        font-weight: bold;
+        line-height: 2rem;
+    `,
+    h6: css`
+        font-family: ${FONT.headingBold};
+        font-size: 1.25rem;
+        font-weight: bold;
+        line-height: 1.75rem;
+    `,
+
+    p1: css`
+        font-family: ${ FONT.main };
+        font-size: 1.25rem;
+        line-height: 1.75rem;
+    `,
+
+    p2: css`
+        font-family: ${ FONT.main };
+        font-size: 1rem;
+        line-height: 1.5rem;
+    `,
+
+    p3: css`
+        font-family: ${ FONT.main };
+        font-size: 0.8125rem;
+        line-height: 1.25rem;
+    `,
+}
